f004597ea9868582d1a5f0423ddbc52abd44fb96
penglong <penglong@oasgame.com>
2019-11-08 18:48:50 +0800

    就当好使

---
+++ HangUpRpg/Assets/WindyFramework/Custom/Data/DataRowProcessor/PropertyExpDropDataRow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using WindyFramework.Data;
+
+namespace WindyFramework.Data
+{
+    public class PropertyExpDropDataRow : ADataRow
+    {
+        public int LevelDiff { get; private set; }
+        public int Exp { get; private set; }
+
+        public override void ParseData(DataHolder dataHolder)
+        {
+            dataHolder.ResetIndex();
+            Id = int.Parse(dataHolder.GetData());
+            LevelDiff = int.Parse(dataHolder.GetData());
+            Exp = int.Parse(dataHolder.GetData());
+        }
+    }
+}
+
---
+++ HangUpRpg/Assets/WindyFramework/Custom/Data/DataRowProcessor/PropertyLevelDataRow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using WindyFramework.Data;
+
+namespace WindyFramework.Data
+{
+    public class PropertyLevelDataRow : ADataRow
+    {
+        public float Property { get; private set; }
+        public int Exp { get; private set; }
+
+        public override void ParseData(DataHolder dataHolder)
+        {
+            dataHolder.ResetIndex();
+            Id = int.Parse(dataHolder.GetData());
+            Property = float.Parse(dataHolder.GetData());
+            Exp = int.Parse(dataHolder.GetData());
+        }
+    }
+}
+
